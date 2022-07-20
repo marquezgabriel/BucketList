54cf5dfa3ab1e8061ec0b8821cedb8ff6c20d2ab
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-07-20 09:11:41 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Gabriel Marquez on 2022-07-20.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
