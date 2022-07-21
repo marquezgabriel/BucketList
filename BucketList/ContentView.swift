b9b269b2f0110edbbb6cfb9717848a4e3d44eefd
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-07-21 14:08:43 -0400

    Integrating MapKit with SwiftUI

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,52 +5,38 @@
 //  Created by Gabriel Marquez on 2022-07-20.
 //
 
+import MapKit
 import SwiftUI
 
-enum LoadingState {
-    case loading, success, failed
-}
-
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success!")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed.")
-    }
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
 }
 
 struct ContentView: View {
-    var loadingState = LoadingState.loading
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
     
     var body: some View {
-        
-//        if loadingState == .loading {
-//            LoadingView()
-//        } else if loadingState == .success {
-//            SuccessView()
-//        } else if loadingState == .failed {
-//            FailedView()
-//        }
-        
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                        Circle()
+                            .stroke(.red, lineWidth: 3)
+                            .frame(width: 44, height: 44)
+                    }
+                }
+            }
+            .navigationTitle("London Explorer")
         }
-        
     }
 }
 
