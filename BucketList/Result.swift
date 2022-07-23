7cd0d085170a25565148ec4458dcda71914ea44a
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-07-23 19:03:05 -0400

    Day 71 - Downloading data from Wikipedia

---
+++ BucketList/Result.swift
@@ -0,0 +1,22 @@
+//
+//  Result.swift
+//  BucketList
+//
+//  Created by Gabriel Marquez on 2022-07-23.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
